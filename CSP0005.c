ada01805d47aced6a4870f3b34e63a1b03e4b3be
Nguyen Long <longnq9@fpt.edu.vn>
2020-01-14 16:00:45 +0700

    Update CSP0005.c

--- CSP0005.c
+++ CSP0005.c
@@ -8,8 +8,12 @@ int main()
     char choice;
 
     do{
-    printf("\nPlease enter a number (1-9): ");
-    scanf("%d",&number);
+    printf("\nPlease enter a number (1-9): "); scanf("%d",&number);
+    while(number < 1 || number > 9)
+    {
+        printf("Please enter a number (1-9): "); scanf("%d",&number);
+    }
+
 
     printf("Multiplication table for %d number\n",number);
     for(i=0;i<=10;i++)
